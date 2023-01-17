0e5d661519fd9ca96ca432af79dc18e965de6e45
Yentl Hendrickx <r0842908@student.thomasmore.be>
2023-01-17 11:38:07 +0100

    basic nav

--- src/App.js
+++ src/App.js
@@ -1,13 +1,61 @@
 import logo from './logo.svg';
 import './App.css';
 
+// Components
 import AuthForm from './components/authform';
+import Home from './components/home';
+
+// Navigation
+import { Routes, Route, BrowserRouter, NavLink } from 'react-router-dom';
+
+
+import { useState, useEffect } from 'react';
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(true);
+
+  useEffect(() => {
+    const loggedIn = localStorage.getItem('isLoggedIn');
+    if (loggedIn) {
+      setIsLoggedIn(true);
+    }
+  }, []);
+
+  const handleLogout = () => {
+    localStorage.removeItem('isLoggedIn');
+    setIsLoggedIn(false);
+  }
+
+  function Header() {
+    return (
+      <div>
+        <h1>Project 4.0</h1>
+        <ul>
+          <li><NavLink end to="" className={({isActive}) => isActive ? "active" : undefined}>Home</NavLink></li>
+          <li><NavLink to="/login" className={({isActive}) => isActive ? "active" : undefined}>Login</NavLink></li>
+          <li><NavLink to="Register" className={({isActive}) => isActive ? "active" : undefined}>Register</NavLink></li>
+        </ul>
+      </div>
+    );
+  }
+
+  function Main() {
+    return (
+      <div>
+        <Routes>
+          <Route path={'/'} element={ <Home/> }/>
+          <Route path={'/login'} element={ <AuthForm forLogin={true} setIsLoggedIn={setIsLoggedIn} /> }/>
+          <Route path={'/register'} element={ <AuthForm forLogin={false} setIsLoggedIn={setIsLoggedIn}/> }/>
+        </Routes>
+      </div>
+    )
+  }
+
   return (
-    <div className="App">
-      <AuthForm />
-    </div>
+    <BrowserRouter>
+      <Header />
+      <Main />    
+    </BrowserRouter>
   );
 }
 
