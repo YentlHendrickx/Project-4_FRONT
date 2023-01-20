4f0d5f45a3cde42ee485ba1bff1b9df961bdebbf
Yentl Hendrickx <71765371+YentlHendrickx@users.noreply.github.com>
2023-01-20 15:20:14 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,7 @@ module.exports = {
     colors: {
       'uiPrimary'     : '#e63946ff', // Imperial Red
       'uiSecondary'   : '#457B9D',   // Celadon Blue
+      'uiSecondaryLight' : '#679DBF', // Celadon Blue lighter?
       'uiLight'       : '#f1faeeff', // Honeydew
       'uiLight2'      : '#A8DADC',   // Powder Blue
       'uiNav'         : '#1D3557',   // Prussian Blue
