a7253127ba934635a3227d1e3507001700d52cfe
Yentl Hendrickx <r0842908@student.thomasmore.be>
2023-01-26 09:29:12 +0100

    yeet

--- src/store.js
+++ src/store.js
@@ -1,9 +1,8 @@
 import { atom } from 'recoil';
 
-
 export const userDataState = atom({
     key: 'userDataState',
-    default: {firstName: "", lastName: "", userId: -1}
+    default: {firstName: "", lastName: "", email: "", userId: -1}
 });
 
 export const initialsState = atom({
