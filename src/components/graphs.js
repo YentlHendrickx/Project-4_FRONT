7bdd9d4dc0ea237a4aebd4a950a9b593e2d571b9
Yentl Hendrickx <r0842908@student.thomasmore.be>
2023-01-27 17:52:12 +0100

    first graph success!

--- src/components/graphs.js
+++ src/components/graphs.js
@@ -1,6 +1,76 @@
-function Graphs() {
-    // nebula = R
+import { useEffect, useState, useRef } from "react";
+import { embed } from "@nebula.js/stardust";
 
+import barchart from '@nebula.js/sn-bar-chart';
+
+
+import { useRecoilValue } from "recoil";
+import { enigmaUrlState } from "../store";
+
+const enigma = require('enigma.js');
+const schema = require('enigma.js/schemas/12.1306.0.json');
+
+
+const config = {
+    host                : process.env.REACT_APP_QLIK_HOST,
+    prefix              : process.env.REACT_APP_QLIK_PREFIX,
+    port                : process.env.REACT_APP_QLIK_PORT,
+    webIntegrationId    : process.env.REACT_APP_QLIK_INTEGRATION_ID,
+    isSecure            : true,
+}
+
+
+function Graphs() {
+    const [graph, setGraph] = useState(null);
+    const ref = useRef(null);
+    const enigmaUrl = useRecoilValue(enigmaUrlState);
+    
+    useEffect(() => {
+        const getEnigma = async () => {
+            const session = await enigma.create({
+                schema,
+                url: enigmaUrl
+            });
+
+            const app = await (await session.open()).openDoc('c16d3353-ee3a-457e-9de8-66f1b0f55c0e');
+
+            return app;
+        }
+
+        const runEnigma = async () => {
+            if (enigmaUrl) {
+                const newEnigmaApp = await getEnigma();
+    
+                const n = embed(newEnigmaApp, {
+                    types: [
+                        {
+                            name: "barchart",
+                            load: () => Promise.resolve(barchart)
+                        }
+                    ]
+                });
+
+                const element = ref.current;
+                
+                n.render({
+                    element,
+                    id: 'mGxVj',
+                });
+            }
+        }
+
+        runEnigma();
+
+    }, [enigmaUrl])
+
+
+    return (
+        <div className="w-screen h-screen">
+           <div className="w-[50%] h-[50%]" ref={ref}>
+
+            </div>
+        </div>
+    );
 }
 
 export default Graphs;
--- src/helpers/qlikConnect.jsx
+++ src/helpers/qlikConnect.jsx
@@ -1,19 +1,28 @@
 import React, { useEffect, useState } from 'react';
+import { useRecoilState } from 'recoil';
+import { enigmaUrlState } from '../store';
+import { Auth, AuthType } from '@qlik/sdk';
+
+const enigma = require('enigma.js');
+const schema = require('enigma.js/schemas/12.612.0.json');
 
 const config = {
-    host                 : process.env.REACT_APP_QLIK_HOST,
+    host                : process.env.REACT_APP_QLIK_HOST,
     prefix              : process.env.REACT_APP_QLIK_PREFIX,
     port                : process.env.REACT_APP_QLIK_PORT,
     webIntegrationId    : process.env.REACT_APP_QLIK_INTEGRATION_ID,
+    appId               : process.env.REACT_APP_QLIK_APP_ID,
+    apiKey              : process.env.REACT_APP_QLIK_API_KEY,
     isSecure            : true,
 }
 
 const QlikConnect = ({ children }) => {
     const [isLoggedIn, setIsLoggedIn] = useState(false);
+    const [enigmaUrl, setEnigmaUrl] = useRecoilState(enigmaUrlState);
 
     useEffect(() => {
         async function login() {
-            fetch(`https://${config.host}/api/v1/users/me`, {
+            return fetch(`https://${config.host}/api/v1/users/me`, {
                 method: 'GET',
                 mode: 'cors',
                 credentials: 'include',
@@ -23,25 +32,53 @@ const QlikConnect = ({ children }) => {
                 }
             }).then(response => {
                 if (response.status === 200) {
-                    console.warn(response);
+                    // console.warn(response);
                     return true;
                 } else {
                     window.location.href = `https://${config.host}/login?qlik-web-integration-id=${config.webIntegrationId}&returnto=${window.location.href}`;
+                    return false;
                 }
             }).catch(error => {
                 console.warn(error);
                 return false;
-            })
+            });
         }
 
-        if (isLoggedIn === false) {
+        if (isLoggedIn === false || enigmaUrlState === null) {
             const loginState = async () => {
-                return await login();
+                const state = await login();
+                // console.log(await state);
+
+                const getCSRF = async () => {
+                    const webIntegrationId = config.webIntegrationId;
+                    const hostUrl = config.host;
+                    const appId = config.appId;
+                    
+                    const authInstance = new Auth({
+                        webIntegrationId,
+                        autoRedirect: true,
+                        authType: AuthType.WebIntegration,
+                        host: hostUrl,
+                    });
+
+                    if (!authInstance.isAuthenticated()) {
+                        authInstance.authenticate();
+                        return "UWU"
+                    } else {
+                        const wssUrl = await authInstance.generateWebsocketUrl(appId);
+                        return wssUrl;
+                    }
+                }
+
+                if (state) {
+                    setIsLoggedIn(true);
+                    const wssUrl = await getCSRF();
+                
+                    setEnigmaUrl(wssUrl);
+                }
             }
 
-            if (loginState() === true) {
-                setIsLoggedIn(true);
-            }
+            loginState();
         }
 
     }, []);
