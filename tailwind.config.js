ed91e3519531a5b83c17b9020413ee950d42b2ca
Yentl Hendrickx <r0842908@student.thomasmore.be>
2023-02-01 16:27:09 +0100

    postcss and autoprefixer

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
-/** @type {import('tailwindcss').Config} */
+// 
 module.exports = {
   content: [
     "./src/App.js",
-    "./src/components/*.{js,jsx,ts,tsx}"
+    "./src/**/*.{js,jsx,ts,tsx}"
   ],
   theme: {
     extend: {
