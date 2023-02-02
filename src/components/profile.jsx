994daf4fa0441656239fba92e113de536969ee44
Yentl Hendrickx <r0842908@student.thomasmore.be>
2023-02-02 14:47:09 +0100

    styling profile page

--- src/components/profile.jsx
+++ src/components/profile.jsx
@@ -8,141 +8,247 @@ import { userDataState } from "../store"
 // Axios
 import axios from "axios"
 
+import {
+    Card,
+    CardHeader,
+    CardBody,
+    CardFooter,
+    Typography,
+} from "@material-tailwind/react";
+
 // Notifications
 import { ReactNotifications, Store } from 'react-notifications-component';
 import 'react-notifications-component/dist/theme.css';
 
-export default function Profile({handleLogout}){
-    const [changeEmail, setChangeEmail] = useState(false)
-    const [changePassword, setChangePassword] = useState(false)
-    const [emailFormData, setEmailFormData] = useState({
-        oldEmail: '',
-        email: '',
-        password: ''
-    })
-    const [passwordFormData, setPasswordFormData] = useState({
-        newPassword: '',
-        oldPassword: '',
-        email: ''
-    })
+export default function Profile ({ handleLogout }) {
     const userState = useRecoilValue(userDataState);
+    console.warn(userState);
 
+    // Form data
+    const [emailFormData, setEmailFormData] = useState({
+        oldEmail:   userState.email,
+        password:   '',
+        email:      '',
+    });
 
-    async function handleChangeEmail(event){
-        event.preventDefault();
-        // setChangeEmail(true)
-        // setChangePassword(false)
-        if(event){
-            const newEmail = {
-                newEmail: emailFormData.newEmail,
-                oldEmail: userState.email,
-                password: emailFormData.password
-            }
-            // console.log(process.env.REACT_APP_API_URL + 'User/changeemail', newEmail);
-            await axios.put(process.env.REACT_APP_API_URL + 'User/changeemail/', newEmail);
-            // console.log(result)
-            // console.log(newEmail)
+    const [emailFormErrors, setEmailFormErrors] = useState({
+        password:   '',
+        email:      '',
+    });
 
-            Store.addNotification({
-                title: "Email Sent!",
-                message: `Email sent to\n${emailFormData.newEmail}.`,
-                type: "success",
-                insert: "center",
-                container: "top-right",
-                animationIn: ["animate__animated", "animate__fadeIn"],
-                animationOut: ["animate__animated", "animate__fadeOut"],
-                dismiss: {
-                  duration: 5000,
-                  onScreen: true,
-                  pauseOnHover: true
+    const [passwordFormData, setPasswordFormData] = useState({
+        newPasswordConfirm: '',
+        newPassword:        '',
+        oldPassword:        '',
+        email:              ''
+    });
+
+    const [passwordFormErrors, setPasswordFormErrors] = useState({
+        newPasswordConfirm: '',
+        newPassword:        '',
+        oldPassword:        '',
+        email:              ''
+    });
+
+    const setFormErrors = (type, name, value) => {
+        console.warn(type, name, value);
+        let errors = emailFormErrors;
+
+        if (type === 'password') {
+            errors = passwordFormErrors;
+        }
+
+        // Add errors when needed
+        switch (name) {
+          case 'email':
+            // Regex for email validation
+            errors.email = value.match(/^([\w.%+-]+)@([\w-]+\.)+([\w]{2,})$/i)
+            ? ''
+            : 'Invalid email address';
+            break;
+
+          case 'oldPassword':
+          case 'newPassword':
+          case 'password':
+            let passwordError = value.length < 8 ? 'Password must be at least 8 characters long' : '';
+            if (name === 'password') {
+                errors.password = passwordError;
+            } else {
+                if (name === 'oldPassword') {
+                    errors.oldPassword = passwordError;
+                } else {
+                    if (passwordError === "") {
+                        errors.newPasswordConfirm = passwordFormData.newPasswordConfirm !== value ? 'Passwords do not match' : '';
+                    }
+    
+                    errors.newPassword = passwordError;
                 }
-              });
-              setTimeout(() => handleLogout(), 5000);
+            }
+            break;
+
+          case 'newPasswordConfirm':
+            errors.newPasswordConfirm = passwordFormData.newPassword !== value ? 'Passwords do not match' : '';
+
+            if (errors.newPasswordConfirm === '') {
+              errors.newPasswordConfirm = value.length < 8 ? 'Password must be at least 8 characters long' : '';
+            }
+            break;
+
+          default:
+            break;
+        }
+    
+        if (type === 'password') {
+            setPasswordFormErrors({
+                email: errors.email,
+                oldPassword: errors.oldPassword,
+                newPassword: errors.newPassword,
+                newPasswordConfirm: errors.newPasswordConfirm,
+            });
+        } else {
+            setEmailFormErrors({
+                email: errors.email,
+                password: errors.password,
+            });
         }
     }
+    
+    // Validate the forms
+    function validate(type) {
+        let valid = true;
+        
+        let errors = emailFormErrors;
 
-    async function handleChangePassword(event){
-        event.preventDefault();
-
-        if(event) {
-            const newPassword = {
-                newPassword: passwordFormData.newPassword,
-                oldPassword: passwordFormData.oldPassword,
-                email: passwordFormData.email
-            }
-
-            await axios.post(process.env.REACT_APP_API_URL + 'User/changepassword/', newPassword)
-
-            Store.addNotification({
-                title: "Password Changed!",
-                message: `Password has been changed`,
-                type: "success",
-                insert: "top",
-                container: "top-right",
-                animationIn: ["animate__animated", "animate__fadeIn"],
-                animationOut: ["animate__animated", "animate__fadeOut"],
-                dismiss: {
-                  duration: 5000,
-                  onScreen: true,
-                }
-              });     
-              
-          setTimeout(() => handleLogout(), 5000);
+        if (type === 'password') {
+            errors = passwordFormErrors; 
         }
+        
+        // Check for any errors strings
+        Object.values(errors).forEach(val => val.length > 0 && (valid = false));
+        
+        return valid;
     }
 
-    function onChangeEmail(event){
-        setEmailFormData({
+    async function handleChangeEmail(event) {
+        event.preventDefault();
+
+        if (!validate('email')) {
+            return;
+        }
+
+        const newEmail = {
+            oldEmail: userState.email,
+            newEmail: emailFormData.newEmail,
+            password: emailFormData.password,
+        }
+
+        await axios.put(process.env.REACT_APP_API_URL + 'User/changeemail/', newEmail)
+        .error(err => {
+            console.warn(err)
+        });
+
+        Store.addNotification({
+            title: "Email Sent!",
+            message: `Email sent to\n${emailFormData.newEmail}.`,
+            type: "success",
+            insert: "center",
+            container: "top-right",
+            animationIn: ["animate__animated", "animate__fadeIn"],
+            animationOut: ["animate__animated", "animate__fadeOut"],
+            dismiss: {
+                duration: 5000,
+                onScreen: true,
+                pauseOnHover: true
+            }
+        });
+
+        setTimeout(() => handleLogout(), 5000);
+    }
+
+    // Handle password change
+    async function handleChangePassword(event) {
+        event.preventDefault();
+
+        if (!validate('password')) {
+            return;
+        }
+
+        const newPassword = {
+            newPassword: passwordFormData.newPassword,
+            oldPassword: passwordFormData.oldPassword,
+            email: passwordFormData.email
+        }
+
+        await axios.post(process.env.REACT_APP_API_URL + 'User/changepassword/', newPassword)
+        .error(err => {
+            console.warn(err);
+        });
+
+        Store.addNotification ({
+            title: "Password Changed!",
+            message: `Password has been changed.`,
+            type: "success",
+            insert: "top",
+            container: "top-right",
+            animationIn: ["animate__animated", "animate__fadeIn"],
+            animationOut: ["animate__animated", "animate__fadeOut"],
+            dismiss: {
+                duration: 5000,
+                onScreen: true,
+            }
+        });     
+            
+        setTimeout(() => handleLogout(), 5000);
+    }
+
+    function onChangeEmail(event) {
+        setEmailFormData ({
             ...emailFormData,
             [event.target.name]: event.target.value
-        })
-        // console.log(emailFormData)
+        });
+
+        setFormErrors('email', event.target.name, event.target.value);
     }
-    function onChangePassword(event){
+
+    function onChangePassword (event) {
         setPasswordFormData({
             ...passwordFormData,
             [event.target.name]: event.target.value
-        })
-        // console.log(passwordFormData)
+        });
+
+        setFormErrors('password', event.target.name, event.target.value);
     }
 
+    return (
+        <div className="w-full h-full">
+            <ReactNotifications className="!relative"/>
 
-    return(<div>
-        <ReactNotifications className="!relative"/>
-                { !changeEmail && !changePassword?(
-                <div className=" flex justify-center gap-8 m-[6vh]">
-                    <button className=" bg-green-500 rounded-lg text-uiLight w-[50rem]" onClick={()=> setChangeEmail(!changeEmail)}>Change email</button>
-                    <button className=" bg-uiPrimary rounded-lg text-uiLight w-[50rem]" onClick={()=> setChangePassword(!changePassword)}>change password</button>
+            <div className="flex flex-col items-center mt-4">
+                <p className="text-4xl">{userState.firstName} {userState.lastName}</p>
+                <p className="text-xl font-light italic">{userState.email}</p>
+
+                <div className="w-full flex flex-col">
+                    <div className="mt-6">
+                        <p className="text-xl text-center">Change email</p>
+                        <form className="flex flex-col mx-2 items-center mt-2 gap-4"  onSubmit={handleChangeEmail}>
+                            <input className="pl-2 w-[40%]" type='email' name={"email"} placeholder={"New email"} value={emailFormData.email} onChange={onChangeEmail}/>
+                            <input className="pl-2 w-[40%]" type='password' name={"password"} placeholder={"Current password"} value={emailFormData.password} onChange={onChangeEmail}/>
+                            <button className="bg-save hover:bg-saveHover text-uiLight rounded-md w-[20%]" type="submit">Change email</button>
+                        </form>
+                    </div>
+
+                    <div className="mt-6">
+                        <p className="text-xl text-center">Change password</p>
+                        <form className="flex flex-col mx-2 items-center mt-2 gap-4"  onSubmit={handleChangePassword}>
+                            <input className="pl-2 w-[40%]" type='email' name={"email"}placeholder={"Email"} value={passwordFormData.email} onChange={onChangePassword}/>
+                            <input className="pl-2 w-[40%]" type='password' name={"oldPassword"} placeholder={"Current password"} value={passwordFormData.oldPassword} onChange={onChangePassword}/>
+                            <input className="pl-2 w-[40%]" type='password' name={"newPassword"} placeholder={"New password"} value={passwordFormData.newPassword} onChange={onChangePassword}/>
+                            <input className="pl-2 w-[40%]" type='password' name={"newPasswordConfirm"} placeholder={"Confirm password"} value={passwordFormData.newPasswordConfirm} onChange={onChangePassword}/>
+                            <button className="bg-save hover:bg-saveHover text-uiLight rounded-md w-[20%]" type="submit">Change password</button>
+                        </form>
+                    </div>
                 </div>
-                ):(<>
-                    { changeEmail ? (
-
-                        <div className="grid grid-cols-1 gap-3 w-[50%] mx-auto">
-                            
-                            <h1 className="text-4xl text-center">Change email</h1>
-
-                            <form className=" grid grid-cols-4 gap-3 mx-[8vh]"  onSubmit={handleChangeEmail}>
-                                <input className=" text-center col-span-4" type='email' name={"newEmail"} placeholder={"New email"} value={emailFormData.newEmail} onChange={onChangeEmail}/>
-                                <input className=" text-center col-span-4" type='password' name={"password"} placeholder={"Password"} value={emailFormData.password} onChange={onChangeEmail}/>
-                                <button className="bg-green-500 text-uiLight rounded-xl col-start-2 col-span-2" type="submit">Change email</button>
-                            </form>
-                        </div>
-                    ):(
-                        <div className="grid grid-cols-1 gap-3 w-[50%] mx-auto">
-                                
-                            <h1 className="text-4xl text-center">Change password</h1>
-
-                            <form className=" grid grid-cols-4 gap-3 mx-[8vh]"  onSubmit={handleChangePassword}>
-                                <input className=" text-center col-span-2" type='password' name={"oldPassword"} placeholder={"Old password"} value={passwordFormData.oldPassword} onChange={onChangePassword}/>
-                                <input className=" text-center col-span-2" type='password' name={"newPassword"} placeholder={"New password"} value={passwordFormData.newPassword} onChange={onChangePassword}/>
-                                <input className=" text-center col-span-4" type='email' name={"email"}placeholder={"Email"} value={passwordFormData.email} onChange={onChangePassword}/>
-                                <button className="bg-green-500 text-uiLight rounded-xl col-start-2 col-span-2" type="submit">Change password</button>
-                            </form>
-                        </div>
-                    )}
-                    </>
-                )
-            }
             </div>
-    )
+        </div>
+    );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,18 +7,18 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        'uiPrimary'     : '#e63946ff', // Imperial Red
-        'uiSecondary'   : '#457B9D',   // Celadon Blue
-        'uiSecondaryLight' : '#679DBF', // Celadon Blue lighter?
-        'uiLight'       : '#f1faeeff', // Honeydew
-        'uiLight2'      : '#A8DADC',   // Powder Blue
-        'uiNav'         : '#1D3557',   // Prussian Blue
-        'edit'          : '#0A8754',   // Green
-        'delete'        : '#c70000',   // Red
-        'deleteHover'   : '#e90000',
-        'create'        : '#3b83f6',
-        'save'          : '#00ab66',
-        'saveHover'     : '#00cd88',
+        'uiPrimary'         : '#e63946ff',  // Imperial Red
+        'uiSecondary'       : '#457B9D',    // Celadon Blue
+        'uiSecondaryLight'  : '#679DBF',    // Celadon Blue lighter?
+        'uiLight'           : '#f1faeeff',  // Honeydew
+        'uiLight2'          : '#A8DADC',    // Powder Blue
+        'uiNav'             : '#1D3557',    // Prussian Blue
+        'edit'              : '#0A8754',    // Green
+        'delete'            : '#c70000',    // Red
+        'deleteHover'       : '#e90000',
+        'create'            : '#3b83f6',
+        'save'              : '#00ab66',
+        'saveHover'         : '#00cd88',
       },
     },
   },
